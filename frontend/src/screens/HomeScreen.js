0954ac3cd45e75a3c2c2f6f815ef09e7ae354eff
Danilo <realdanilo@users.noreply.github.com>
2020-10-04 15:32:08 -0700

    set product fetch, and proxy

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import products from '../products'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import axios from "axios";
 
 const HomeScreen = () => {
-    return (
-        <>
-            <h1>Lates Product</h1>
-            <Row>
-                {products.map((p, i) => (
-                    <Col key={i} sm="12" md="6" lg="4" xl="3">
-                        <Product product={p} />
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
-}
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const loadData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    loadData();
+  }, []);
+  return (
+    <>
+      <h1>Lates Product</h1>
+      <Row>
+        {products.map((p, i) => (
+          <Col key={i} sm="12" md="6" lg="4" xl="3">
+            <Product product={p} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
