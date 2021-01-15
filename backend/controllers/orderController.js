6f2414f3369a2839237d065a843b74eebb606b27
Danilo <realdanilo@users.noreply.github.com>
2021-01-14 20:54:13 -0700

    add backend function to update product delivery

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,8 +57,8 @@ const updateOrderToPaid = asyncHandler(async (req,res)=>{
             update_time:req.body.update_time,
             email_address:req.body.payer.email_address
         }
-        await order.save()
-        res.json(order)
+       const updatedOrder=  await order.save()
+        res.json(updatedOrder)
     }else{
         res.status(404)
         throw new Error("Order not found")
@@ -90,7 +90,24 @@ const updateOrderToPaid = asyncHandler(async (req,res)=>{
  })
 
 
+//description: Update order to delivered
+//route: GET /api/order/:id/deliver
+//access: private/admin
+const updatedOrderToDelivered = asyncHandler(async (req,res)=>{
+    const order = await Order.findById(req.params.id)
+ 
+    if(order){
+        order.isDelivered = true
+        order.deliveredAt = Date.now()
+       
+        const updatedOrder = await order.save()
+        res.json(updatedOrder)
+    }else{
+        res.status(404)
+        throw new Error("Order not found")
+    }
+ })
 
 
 
-export { addOrderItems, getOrderByID,updateOrderToPaid,getMyOrders, getOrders}
+export { addOrderItems, getOrderByID,updateOrderToPaid,getMyOrders, getOrders,updatedOrderToDelivered}
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,12 +1,13 @@
 import express from "express";
 const router = express.Router();
 import {protect,admin} from "../middleware/auth.js"
-import {addOrderItems, getOrderByID,updateOrderToPaid,getMyOrders, getOrders} from "../controllers/orderController.js"
+import {addOrderItems, getOrderByID,updateOrderToPaid,getMyOrders, getOrders, updatedOrderToDelivered} from "../controllers/orderController.js"
 
 router.route("/").post(protect,addOrderItems).get(protect,admin,getOrders)
 router.route("/myorders").get(protect,getMyOrders)
 router.route("/:id").get(protect,getOrderByID)
 router.route("/:id/pay").put(protect,updateOrderToPaid)
+router.route("/:id/deliver").put(protect,admin, updatedOrderToDelivered)
 
 
 export default router;  
